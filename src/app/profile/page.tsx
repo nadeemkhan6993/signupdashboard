716f00046f56cdc08eaca1a186b0a1e3cb588893
Nadeem Akram <nadeemkhan6993@gmail.com>
2024-10-08 12:54:41 +0530

    profile page is updated

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 function Profile() {
   return (
-    <div>Profile Page</div>
+    <div className="flex min-h-screen flex-col items-center p-24">
+        <h1>Hi, Welcome to your profile</h1>
+    </div>
   )
 }
 
